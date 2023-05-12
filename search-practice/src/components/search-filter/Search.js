275545eb89ae4a4dd9b230e94312911cc9a4d307
Nhoj <nhojplummer@yahoo.com>
2023-05-12 10:46:29 -0400

    Trying to build this challenge again

--- search-practice/src/components/search-filter/Search.js
+++ search-practice/src/components/search-filter/Search.js
@@ -3,37 +3,14 @@ import React, { useState } from "react"
 const Search = () => {
   const [searchInput, setSearchInput] = useState("")
 
-  // const fruits = ["Banana",  "Apple", "Orange", "Mango", "Pineapple", "Watermelon"]
+  const fruits = ["Banana", "Apple", "Orange", "Mango", "Pineapple", "Watermelon"]
 
-  const fruits = [
-    { id: 1, item: "Banana" },
-    { id: 2, item: "Apple" },
-    { id: 3, item: "Orange" },
-    { id: 4, item: "Mango" },
-    { id: 5, item: "Pineapple" },
-    { id: 6, item: "Watermelon" }
-  ]
-
-  const handleChange = e => {
-    e.preventDefault()
-    setSearchInput(e.target.value)
-    if (searchInput.length > 0) {
-      fruits.filter(fruit => {
-        return fruit.item.match(searchInput)
-      })
-    }
-  }
-  console.log(searchInput)
-
+  
   return (
     <div>
-      <input type="text" placeholder="Testing Here" onChange={handleChange} value={searchInput} />
+      <input type="text" placeholder="Search..." />
       <table>
-        {fruits.map(fruit => (
-          <tr key={fruit.id}>
-            <td>{fruit.fruit}</td>
-          </tr>
-        ))}
+        
       </table>
     </div>
   )
