71c04e9c76bba7039404b18ca926729e8713037f
Nhoj <nhojplummer@yahoo.com>
2023-05-12 11:12:33 -0400

    Hope this way works this time

--- search-practice/src/components/search-filter/Search.js
+++ search-practice/src/components/search-filter/Search.js
@@ -3,15 +3,23 @@ import React, { useState } from "react"
 const Search = () => {
   const [searchInput, setSearchInput] = useState("")
 
-  const fruits = ["Banana", "Apple", "Orange", "Mango", "Pineapple", "Watermelon"]
+  // const fruits = ["Banana", "Apple", "Orange", "Mango", "Pineapple", "Watermelon"]
 
-  
+  const fruits = [
+    { id: 1, fruit: "Banana" },
+    { id: 2, fruit: "Apple" },
+    { id: 3, fruit: "Orange" },
+    { id: 4, fruit: "Mango" },
+    { id: 5, fruit: "Pineapple" },
+    { id: 6, fruit: "Watermelon" }
+  ]
+
   return (
     <div>
       <input type="text" placeholder="Search..." />
-      <table>
-        
-      </table>
+      {fruits.map(item => {
+        return <div> {item.fruit}</div>
+      })}
     </div>
   )
 }
